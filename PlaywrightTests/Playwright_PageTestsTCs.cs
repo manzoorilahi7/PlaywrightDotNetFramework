4083e37aa210d86f7c2e9f98b44a88d57baab295
Manzoor Ilahi <manzoorilahi21@gmail.com>
2024-01-21 10:07:17 +0530

    Added more methods

---
+++ PlaywrightTests/Playwright_PageTestsTCs.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlaywrightTests
+{
+    [TestFixture]
+    public class Playwright_PageTestsTCs : PageTest
+    {
+        [Test]
+        public async Task LoginTestUsingPageTest()
+        {
+            await Page.GotoAsync("https://opensource-demo.orangehrmlive.com/");
+            await Expect(Page).ToHaveTitleAsync("OrangeHRM");
+            await Page.GetByPlaceholder("Username").FillAsync("Admin");
+            await Page.GetByPlaceholder("Password").FillAsync("admin123");
+            await Page.GetByRole(AriaRole.Button, new() { Name = "Login" }).ClickAsync();
+            await Expect(Page).ToHaveURLAsync("https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index");
+        }
+    }
+}
--- PlaywrightTests/UnitTest1.cs
+++ PlaywrightTests/UnitTest1.cs
@@ -132,7 +132,7 @@ namespace PlaywrightTests
             await page.GotoAsync("https://demo.spreecommerce.org/login");
             await page.FillAsync("#spree_user_email", "manzoorchamp91@gmail.com");
             await page.FillAsync("#spree_user_password", "shopping123");
-            //await page.GetByRole(AriaRole.Button, new() { Name = "commit" }).ClickAsync();
+            await page.GetByRole(AriaRole.Button, new() { Name = "Login" }).ClickAsync();
 
 
             await context.Tracing.StopAsync(new()
@@ -142,6 +142,63 @@ namespace PlaywrightTests
 
             await context.CloseAsync();
             await browser.CloseAsync();
+        }
+
+        [Test]
+        public async Task SaveStateTC()
+        {
+            var playwright = await Playwright.CreateAsync();
+            var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions { Headless = false, Timeout = 80000 });
+            var context = await browser.NewContextAsync();
+            var page = await context.NewPageAsync();
+
+            await page.GotoAsync("https://opensource-demo.orangehrmlive.com/");
+            await page.GetByPlaceholder("Username").FillAsync("Admin");
+            await page.GetByPlaceholder("Password").FillAsync("admin123");
+            await page.GetByRole(AriaRole.Button, new() { Name = "Login" }).ClickAsync();
+
+            await context.StorageStateAsync(new()
+            {
+                Path = @"state\state_login.json"
+            });
+
+            await context.CloseAsync();
+            await browser.CloseAsync();
+        }
+
+        [Test]
+        public async Task RetrieveStateTC()
+        {
+            var playwright = await Playwright.CreateAsync();
+            var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions { Headless = false, Timeout = 80000 });
+            var context = await browser.NewContextAsync(new()
+            {
+                StorageStatePath = @"state\state_login.json"
+            });
+            
+            var page = await context.NewPageAsync();
+            await page.GotoAsync("https://opensource-demo.orangehrmlive.com/web/index.php/admin/viewSystemUsers");
+            var locator = page.GetByRole(AriaRole.Button, new() { Name = "Search"});
+
+            await Assertions.Expect(locator).ToHaveTextAsync("Search");
+
+        }
+
+        [Test]
+        public async Task Login_CodeGenTC()
+        {
+            var playwright = await Playwright.CreateAsync();
+            var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions { Headless = false, Timeout = 80000 });
+            var context = await browser.NewContextAsync();
+            var Page = await context.NewPageAsync();
+
+            await Page.GotoAsync("https://demo.spreecommerce.org/login");
+            await Page.GetByPlaceholder("Email").ClickAsync();
+            await Page.GetByPlaceholder("Email").FillAsync("manzoorchamp91@gmail.com");
+            await Page.GetByPlaceholder("Password").ClickAsync();
+            await Page.GetByPlaceholder("Password").FillAsync("shopping123");
+            await Page.GetByRole(AriaRole.Button, new() { Name = "Login" }).ClickAsync();
+            await Page.GetByText("Logged in successfully").ClickAsync();
         }
     }
 }
